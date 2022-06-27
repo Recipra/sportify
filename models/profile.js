e331d9c85e869e3dfc22598a688aa2a6163ac48a
recipra <jmo0425@outlook.com>
2022-06-27 14:08:02 -0400

    adjust models and basketball index

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,20 @@
 import mongoose from 'mongoose'
 
-const profileSchema = new mongoose.Schema({
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  content: String,
+  rating: Number,
+  owner: {type: Schema.Types.ObjectId, ref: 'Profile'}
+}, {
+  timestamps: true
+})
+
+const profileSchema = new Schema({
   name: String,
   avatar: String,
+  roster: [{type: Schema.Types.ObjectId, ref: 'Player'}],
+  reviews: [reviewSchema]
 }, {
   timestamps: true
 })
