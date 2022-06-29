47ad4d4f02ad631447210ae55859757189a27fb2
recipra <jmo0425@outlook.com>
2022-06-29 13:59:53 -0400

    add formatting to add player functionality

--- controllers/players.js
+++ controllers/players.js
@@ -20,6 +20,16 @@ function newPlayer(req, res) {
 }
 
 function create(req, res) {
+  const playerName = `${req.body.name}`
+  const formattedName = playerName.toLowerCase().replace(/\w\S*/g, (text) => {
+    return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase()
+  })
+  const playerTeam = `${req.body.team}`
+  const formattedTeam = playerTeam.toLowerCase().replace(/\w\S*/g, (text) => {
+    return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase()
+  })
+  req.body.name = formattedName
+  req.body.team = formattedTeam
   Player.create(req.body)
   .then(player => {
     res.redirect('/players/new')
